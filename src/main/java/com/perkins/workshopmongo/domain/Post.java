7c66ef3491b913f7c54058e2da70f9281dc8210b
PerkinsFerreira <perkins.ferreira@gmail.com>
2020-05-08 15:20:33 -0300

    Acrescentando comentários aos posts

--- src/main/java/com/perkins/workshopmongo/domain/Post.java
+++ src/main/java/com/perkins/workshopmongo/domain/Post.java
@@ -1,12 +1,15 @@
 package com.perkins.workshopmongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import com.perkins.workshopmongo.dto.AuthorDTO;
+import com.perkins.workshopmongo.dto.CommentDTO;
 
 @Document
 public class Post implements Serializable{
@@ -18,6 +21,7 @@ public class Post implements Serializable{
 	private String title;
 	private String body;
 	private AuthorDTO author;
+	private List<CommentDTO> comment = new ArrayList<CommentDTO>();
 	
 	public Post() {		
 	}
@@ -70,6 +74,14 @@ public class Post implements Serializable{
 	public void setAutor(AuthorDTO autor) {
 		this.author = author;
 	}
+
+	public List<CommentDTO> getComment() {
+		return comment;
+	}
+
+	public void setComment(List<CommentDTO> comment) {
+		this.comment = comment;
+	}	
 	
 	@Override
 	public int hashCode() {
@@ -95,6 +107,8 @@ public class Post implements Serializable{
 			return false;
 		return true;
 	}
+
+
 
 
 	

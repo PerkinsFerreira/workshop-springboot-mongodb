06a6c447d429521e0b9764c828b5d0826898e3df
PerkinsFerreira <perkins.ferreira@gmail.com>
2020-05-08 11:54:06 -0300

    Endpoint para retornar os posts de um usuário

--- src/main/java/com/perkins/workshopmongo/resources/UserResource.java
+++ src/main/java/com/perkins/workshopmongo/resources/UserResource.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.perkins.workshopmongo.domain.Post;
 import com.perkins.workshopmongo.domain.User;
 import com.perkins.workshopmongo.dto.UserDTO;
 import com.perkins.workshopmongo.services.UserService;
@@ -59,6 +60,12 @@ public class UserResource {
 		user.setId(id);
 		user = service.update(user);
 		return ResponseEntity.noContent().build();
+	}
+	
+	@RequestMapping(value="/{id}/posts", method=RequestMethod.GET)
+	public ResponseEntity<List<Post>> findPosts(@PathVariable String id){
+		User user = service.findById(id);
+		return ResponseEntity.ok().body(user.getPosts());
 	}
 
 }

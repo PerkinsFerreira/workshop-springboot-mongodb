b87a219b0a900f0e6768b36a124defa6316e064c
PerkinsFerreira <perkins.ferreira@gmail.com>
2020-05-08 16:33:36 -0300

    Consulta simples com @Query

--- src/main/java/com/perkins/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/perkins/workshopmongo/repository/PostRepository.java
@@ -3,12 +3,16 @@ package com.perkins.workshopmongo.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.perkins.workshopmongo.domain.Post;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String>{
+	
+	@Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+	public List<Post> findByTitle(String text);
 	
 	public List<Post> findByTitleContainingIgnoreCase(String text);
 
